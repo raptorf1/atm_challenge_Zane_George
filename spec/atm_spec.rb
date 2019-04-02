332e81d982227d0f509dee56aac82119378643b8
raptorf1 <raptor_f1@hotmail.com>
2019-04-02 14:06:59 +0200

    Adds spec to check card expiration and relevant private method

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -2,13 +2,19 @@ require "./lib/atm.rb"
 require "date"
 
 describe Atm do
-    let(:account) {instance_double("Account", pin_code: "1234")}
+    let(:account) {instance_double("Account", pin_code: "1234", exp_date: "04/20")}
 
     before do
         allow(account).to receive(:balance).and_return(100)
         allow(account).to receive(:balance=)
     end
 
+    it "reject withdraw if card is expired" do
+        allow(account).to receive(:exp_date).and_return("12/15")
+        expected_output = {status: false, message: "card expired", date: Date.today}
+        expect(subject.withdraw(50, "1234", account)).to eq expected_output
+    end
+    
     it "has $1000 on initialize" do
         expect(subject.funds).to eq 1000
     end
@@ -19,8 +25,8 @@ describe Atm do
     end
 
     it "allow withdraw if account has enough balance" do
-        expected_output = {status: true, message: "success", date: Date.today, amount: 45}
-        expect(subject.withdraw(45, "1234", account)).to eq expected_output
+        expected_output = {status: true, message: "success", date: Date.today, amount: 75}
+        expect(subject.withdraw(75, "1234", account)).to eq expected_output
     end
 
     it "rejects withdraw if account has insufficient funds" do
@@ -38,8 +44,6 @@ describe Atm do
         expected_output = {status: false, message: "wrong pin", date: Date.today}
         expect(subject.withdraw(50, 9999, account)).to eq expected_output
     end
-
-
-
+   
 
 end
