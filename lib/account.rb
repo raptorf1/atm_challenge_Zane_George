82908d8a571aecce3e235148953dc4ca5772ae2d
raptorf1 <raptor_f1@hotmail.com>
2019-04-03 13:40:50 +0200

    Adds unit test for account status, defines instance variable account status in Account class

--- lib/account.rb
+++ lib/account.rb
@@ -1,11 +1,16 @@
 class Account
-    attr_accessor :pin_number, :exp_date
+    attr_accessor :pin_number, :exp_date, :account_status
     STANDARD_VALIDITY_YRS = 5
 
     def initialize
         @pin_number = rand(1000..9999)
         @exp_date = set_expire_date
+        @account_status = :active
     end
+
+    
+    
+    private
 
     def set_expire_date
         stringedDate = Date.today.next_year(STANDARD_VALIDITY_YRS).to_s
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -13,4 +13,8 @@ describe Account do
         expect(subject.exp_date).to eq expected_date
     end
 
+    it "is expected to have :active status on initialize" do
+        expect(subject.account_status).to eq :active
+    end
+
 end
