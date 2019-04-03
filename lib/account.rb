1f9e6f2363fa33279395c07264ef7ec11ae957dc
raptorf1 <raptor_f1@hotmail.com>
2019-04-03 16:12:39 +0200

    Defines private method for pin assignment

--- lib/account.rb
+++ lib/account.rb
@@ -3,7 +3,7 @@ class Account
     STANDARD_VALIDITY_YRS = 5
 
     def initialize
-        @pin_number = rand(1000..9999)
+        @pin_number = assign_pin
         @exp_date = set_expire_date
         @account_status = :active
     end
@@ -18,6 +18,10 @@ class Account
     def set_expire_date
         stringedDate = Date.today.next_year(STANDARD_VALIDITY_YRS).to_s
         stringedDate[5]+stringedDate[6]+"-"+stringedDate[2]+stringedDate[3]
+    end
+
+    def assign_pin
+        @pin_number = rand(1000..9999)
     end
 
     
